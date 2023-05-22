3fdaf43cb8de0d533a816b9fad68cabef97bbd15
Antara Mutalik <antaramutalik@gmail.com>
2023-05-22 14:52:00 -0700

    Co-authored-by: sandyylee <sandyylee@users.noreply.github.com>

--- road.java
+++ road.java
@@ -1,35 +1,71 @@
-import java.awt.*;
-import java.io.*;
+
 import javax.swing.*;
-import javax.swing.JFrame.*;
+import java.awt.*;
+import java.util.*;
 import java.awt.event.*;
+import javax.swing.Timer.*;
 
 
-public class road extends JPanel {
-    //implements MouseListener,MouseMotionListener
-   
+
+ 
 
 
-    private JFrame myFrame;
-    private ImageIcon myPic;
+public class road extends JPanel implements ActionListener  {
+    private int objX = 600; // stores x of obstacle
+    private int ballY; //stores y of character
+    private int windowWidth = 600;
+    private int windowHeight = 400;
 
 
     public road(){
-        myFrame = new JFrame("GeoDash");
-        JFrame.setSize(30, 30);
-        myFrame.add(this);
-        
-        
-
-        // these eyes fit right on Einstein's head.  
-        
-       // addMouseListener(this);
-       // addMouseMotionListener(this);
-        myFrame.setVisible(true);
+        JFrame easel = new JFrame();
+        easel.setSize(windowWidth, windowHeight);
+        easel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        easel.add(this);
+        easel.setVisible(true);
 
 
+        javax.swing.Timer timer = new javax.swing.Timer(100, this);
+        timer.start(); 
+
+      
+    }
+
+    public void paintComponent (Graphics g){
+
+        g.drawLine(0, 250, 600, 250); // road
+
+        g.drawRect(objX, 240, 10, 10); // short obstacle
+        g.drawRect(objX, 230, 10, 20); // tall obstacle
+    
+        //         (x,, y - side length, side length, side length)
+
+        g.drawOval(100, ballY, 20, 20); // character
+
+        
+
+
+
+
+        objX -= 3;
         
     }
+    
+
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+        
+    }
+    
+
+        
+
+
+
+    
+
+    }
+   
 
 
 
@@ -37,4 +73,4 @@ public class road extends JPanel {
 
    
     
-}
+
