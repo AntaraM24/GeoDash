219df0ffc268120ae2fee05a4d6d179dfb34a1ee
Antara Mutalik <antaramutalik@gmail.com>
2023-05-24 19:20:04 -0700

    hk

--- ball.java
+++ ball.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.event.*;
 import javax.swing.Timer.*;
-
+//import java.lang.Object.*;
 
 
 public class ball{
@@ -20,22 +20,27 @@ public class ball{
     public void draw (Graphics g){
         g.drawOval(100, ballY, 20, 20); // character
 
-
-
     }
 
     public void jump(Graphics g){
         ballY = 210;
         draw(g);
+        Thread.sleep(1000);
         ballY = 190;
         draw(g);
         ballY = 170;
         draw(g);
         ballY = 150;
         draw(g);
-        ballY = 0;
+        ballY = 130;
         draw(g);
-        ballY = 220;
+        ballY = 150;
+        draw(g);
+        ballY = 170;
+        draw(g);
+        ballY = 190;
+        draw(g);
+        ballY = 210;
         draw(g);
 
     }
